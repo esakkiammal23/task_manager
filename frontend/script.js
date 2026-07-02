ea451d78e3e7580861b2ec72aee8bbe2a95b62fd
esakkiammal23 <snowmoon2302gmail.com>
2026-07-02 11:09:54 +0530

    production frontend api

--- frontend/script.js
+++ frontend/script.js
@@ -1,5 +1,6 @@
 console.log("Script Loaded");
-const API_URL = "http://127.0.0.1:8000/api";
+const API_URL =
+"https://task-manager-checklist.onrender.com/api";
 loadTasks();
 
 async function loadTasks(){
